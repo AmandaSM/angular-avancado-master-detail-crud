5862f0a4f8c0a8c5888226ef39f74a5cb493ee09
Amanda <ti.amandamonteiro@gmail.com>
2021-04-20 14:59:27 -0300

    refactoing EntryFormComponent

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validator, Validators } from "@angular/forms";
-import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { Entry } from '../shared/entry.model';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Validators } from "@angular/forms";
 
-import toastr from "toastr";
+import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form/base-resource-form.component';
+
+import { Entry } from '../shared/entry.model';
 import { EntryService } from '../shared/Entry.service';
+
 import { PrimeNGConfig } from 'primeng/api';
+
 import { Category } from '../../categories/shared/category.model';
 import { CategoryService } from '../../categories/shared/category.service';
+
 @Component({
   selector: 'app-entry-form',
   templateUrl: './entry-form.component.html',
   styleUrls: ['./entry-form.component.css']
 })
-export class EntryFormComponent implements OnInit, AfterContentChecked {
+export class EntryFormComponent extends BaseResourceFormComponent<Entry> implements OnInit{
 
-  currentAction: string; //edit or create
-  entryForm: FormGroup;
-  pageTitle: string;
-  serverErrorMessages: string[] = null;
-  submittingForm: boolean = false;
-  entry: Entry = new Entry();
   categories: Array<Category>;
 
   imaskConfig = {
@@ -34,19 +30,16 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   };
 
   constructor(
-    private entryService: EntryService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private config: PrimeNGConfig,
-    private categoryService: CategoryService
-  ) { }
-
+    protected entryService: EntryService,
+    protected config: PrimeNGConfig,
+    protected categoryService: CategoryService, 
+    protected injector: Injector
+  ) { 
+    super(injector, new Entry(), entryService, Entry.fromJson)
+  }
   ngOnInit(): void {
 
-    this.setCurrentAction();//action executing
-    this.buildEntryForm();//construir form
-    this.loadEntry();//carregar novamente a lista de categorias
+    super.ngOnInit();
     this.loadCategoires();
 
     //config locale calendar
@@ -66,27 +59,6 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
 
   }
 
-
-  ngAfterContentChecked() {
-    this.setPageTitle();
-  }
-
-  submitForm() {
-
-    this.submittingForm = true;
-
-    if (this.currentAction == "new") {
-
-      this.createEntry();
-
-    } else {
-
-      //== "edit"
-      this.updateEntry();
-    }
-
-  }
-
   get typeOptions(): Array<any> {
     return Object.entries(Entry.types).map(
       ([value, text]) => {
@@ -100,18 +72,9 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
 
   // Private Methods
 
-  private setCurrentAction() {
-    if (this.route.snapshot.url[0].path == "new") {
-      //take content that is in the url [0] 
-      // /entries/new
-      this.currentAction = "new"
-    } else {
-      this.currentAction = "edit"
-    }
-  }
 
-  private buildEntryForm() {
-    this.entryForm = this.formBuilder.group({
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
@@ -123,21 +86,6 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     });
   }
 
-  private loadEntry() {
-    if (this.currentAction == "edit") {
-
-      this.route.paramMap.pipe(
-        switchMap(params => this.entryService.getById(+params.get("id")))//take from url/route the value of id 
-      )
-        .subscribe(
-          (entry) => {
-            this.entry = entry;
-            this.entryForm.patchValue(entry)
-          },
-          (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
-        )
-    }
-  }
 
   private loadCategoires() {
 
@@ -147,63 +95,13 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
 
   }
 
-  private setPageTitle() {
-    if (this.currentAction == 'new') {
-      this.pageTitle = "Cadastro de Nova Lançamento"
-    }
-    else {
-      const entryName = this.entry.name || "" //after content roda 1 vez, antes de carregar categoria 
-      this.pageTitle = "Editando Lançamento: " + entryName;
-    }
+
+  protected editionPageTitle(): string {
+    const categoryName = this.resource.name || "";//if dont't have anything to show, will show ("")
+    return "Editando Categoria: " + categoryName;
   }
 
-  private createEntry() {
-
-    const entry: Entry =Entry.fromJson(this.entryForm.value);
-
-    this.entryService.create(entry)
-      .subscribe(
-        entry => this.actionsForSucess(entry),
-        error => this.actionsForError(error)
-      )
-  }
-
-  private updateEntry() {
-    const entry: Entry =Entry.fromJson(this.entryForm.value);
-
-    this.entryService.update(entry)
-      .subscribe(
-        entry => this.actionsForSucess(entry),
-        error => this.actionsForError(error)
-      )
-  }
-
-  private actionsForSucess(entry: Entry) {
-    toastr.success('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-sunglasses-fill" viewBox="0 0 16 16">    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM2.31 5.243A1 1 0 0 1 3.28 4H6a1 1 0 0 1 1 1v.116A4.22 4.22 0 0 1 8 5c.35 0 .69.04 1 .116V5a1 1 0 0 1 1-1h2.72a1 1 0 0 1 .97 1.243l-.311 1.242A2 2 0 0 1 11.439 8H11a2 2 0 0 1-1.994-1.839A2.99 2.99 0 0 0 8 6c-.393 0-.74.064-1.006.161A2 2 0 0 1 5 8h-.438a2 2 0 0 1-1.94-1.515L2.31 5.243zM4.969 9.75A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .866-.5z"/>  </svg>', 'Solicitação processada com sucesso!');
-  
-    // nomedosite.com/entries/new
-    // nomedosite.com/entries/
-    // nomedosite.com/entries/:id/edit
-
-    //redirect/reload componenet page
-
-    this.router.navigateByUrl("entries", { skipLocationChange: true })//don't show up in to the browser history
-      .then(
-        () => this.router.navigate(["entries", entry.id, "edit"])
-      )
-  }
-
-  private actionsForError(error) {
-
-    toastr.error('Ocorreu um erro ao processar a sua solicitação, <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-dizzy-fill ml-2" viewBox="0 0 16 16">    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM4.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zm5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zM8 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>  </svg>');
-    this.submittingForm = false;
-
-    if (error.status === 422) {//error when server can´t save date
-      this.serverErrorMessages = JSON.parse(error._body).errors; // return [strings]
-    } else {
-      //conection server error
-      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente novamente mais tarde."]
-    }
-
+  protected creationPageTitle(): string {
+    return 'Cadastro de Nova Categoria';
   }
 }
